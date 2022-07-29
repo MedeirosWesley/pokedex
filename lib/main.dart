a0b449bcf6891ff84b593eae35dea3410765c434
MedeirosWesley <wesleymcruz03@gmail.com>
2022-07-29 08:24:24 -0300

    Adição dos cards com o nome e imagem na tela principal

--- lib/main.dart
+++ lib/main.dart
@@ -40,60 +40,104 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Positioned(
-                  //     child: Image.asset(
-                  //   'assets/Pokebola.jpg',
-                  //   height: size.height * .3,
-                  // )),
-                  child: Container(height: size.height * .3)),
-            ],
-          ),
-          Expanded(
-            child: ValueListenableBuilder(
-              valueListenable: store,
-              builder: (_, state, child) {
-                if (state is LoadingPokemonState) {
-                  return Center(
-                    child: Column(
-                      children: const [
-                        Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Text(
-                            "Buscando Dados",
-                            style: TextStyle(
-                                fontSize: 36, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        CircularProgressIndicator(),
-                      ],
-                    ),
-                  );
-                }
-                if (state is ErrorPokemonState) {
-                  return Center(
-                    child: Text(state.message),
-                  );
-                }
-                if (state is SuccessPokemonState) {
-                  return GridView.builder(
-                      itemCount: state.pokemons.length,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 3),
-                      itemBuilder: (_, index) {
-                        final pokemon = state.pokemons[index];
-                        return Image.network(pokemon.sprite);
-                      });
-                }
-                return Container();
-              },
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Positioned(
+                    //     child: Image.asset(
+                    //   'assets/Pokebola.jpg',
+                    //   height: size.height * .3,
+                    // )),
+                    child: Container(height: size.height * .3)),
+              ],
             ),
-          ),
-        ],
+            Expanded(
+              child: ValueListenableBuilder(
+                valueListenable: store,
+                builder: (_, state, child) {
+                  if (state is LoadingPokemonState) {
+                    return Center(
+                      child: Column(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Text(
+                              "Buscando Dados",
+                              style: TextStyle(
+                                  fontSize: 36, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          CircularProgressIndicator(),
+                        ],
+                      ),
+                    );
+                  }
+                  if (state is ErrorPokemonState) {
+                    return Center(
+                      child: Text(state.message),
+                    );
+                  }
+                  if (state is SuccessPokemonState) {
+                    return GridView.builder(
+                        itemCount: state.pokemons.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3),
+                        itemBuilder: (_, index) {
+                          final pokemon = state.pokemons[index];
+                          return Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Container(
+                              padding: const EdgeInsets.all(8),
+                              constraints: BoxConstraints(
+                                  maxWidth: (size.width - 50) / 3),
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(20),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey[500]!,
+                                      offset: const Offset(4, 4),
+                                      blurRadius: 15,
+                                      spreadRadius: 1,
+                                    ),
+                                    const BoxShadow(
+                                      color: Colors.white,
+                                      offset: Offset(-4, -4),
+                                      blurRadius: 15,
+                                      spreadRadius: 1,
+                                    )
+                                  ]),
+                              child: Column(
+                                children: [
+                                  Image.network(
+                                    pokemon.sprite,
+                                    width: (size.width - 40) / 3 - 45,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 8.0),
+                                    child: Text(
+                                      pokemon.name,
+                                      style: const TextStyle(
+                                          fontSize: 10,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                  }
+                  return Container();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
